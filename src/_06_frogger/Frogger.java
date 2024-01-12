b2c672dd6e2af0c70ae541b031401a477b14928f
Sashrik Selva <sashrikselva@gmail.com>
2024-01-11 18:27:57 -0800

    Continue Frogger

--- src/_06_frogger/Frogger.java
+++ src/_06_frogger/Frogger.java
@@ -1,23 +1,19 @@
 package _06_frogger;
 
 import processing.core.PApplet;
+import processing.core.PImage;
 
 public class Frogger extends PApplet {
     static final int WIDTH = 800;
     static final int HEIGHT = 600;
     static final int x = 40;
     static final int y = 40;
+    PImage back;
+    PImage carLeft;
+    PImage carRight;
+    PImage frog;
     
-    
-    public class Frogger
-    {
-    	 x2 = 50;
-    	y2 = 50;
-    	size = 50;
-    	speed=50;
-    }
-    
-    @Override
+     @Override
     public void settings() {
         size(WIDTH, HEIGHT);
         
@@ -27,15 +23,39 @@ public class Frogger extends PApplet {
     @Override
     public void setup() {
 
+    	
+  
+    	
+    	
+    	
+    	back = loadImage("froggerBackground.png");
+        carLeft = loadImage("carLeft.png");
+        carLeft.resize(160,100);
+        carRight = loadImage("carRight.png");
+        carRight.resize(160,100);
+        frog = loadImage("frog.png");
+        frog.resize(75,75);
+        
+        
+        
+        
     }
 
     @Override
     public void draw() {
 
-    	background(red, green, blue);
-    	fill(red, green, blue);
+    	background(10, 30, 30);
+    	
+    	fill(0, 100, 0);
     	
     	ellipse(x,y, width, height);
+    	
+    	background(back);
+        image (carLeft,250,360);
+        image (carRight,250, 210);
+        image (frog, 300, 530);
+        
+        
     }
     static public void main(String[] args) {
         PApplet.main(Frogger.class.getName());
@@ -47,18 +67,23 @@ public class Frogger extends PApplet {
             if(keyCode == UP)
             {
                 //Frog Y position goes up
+            	y= y+1;
             }
             else if(keyCode == DOWN)
             {
                 //Frog Y position goes down 
+            	y= y-1;
             }
             else if(keyCode == RIGHT)
             {
                 //Frog X position goes right
+            	
+            	x= x+1;
             }
             else if(keyCode == LEFT)
             {
                 //Frog X position goes left
+            	x= x-1;
             }
         }
     }
@@ -75,8 +100,43 @@ public class Frogger extends PApplet {
     	}
     }
     
-    void Car(int x2, int y2, int size, int speed)
+    class Car()
     {
+    	int x2;
+    	int y2; 
+    	int size;
+    	int speed;
+    	
+    
+    	void display()
+    	  {
+    	    fill(0,255,0);
+    	    rect(x , y,  size, 50);
+    	  }
+    	
+    	void getX()
+    	{
+    		
+    	}
+    	void getY()
+    	{
+    		
+    	}
+    	void getSize()
+    	{
+    		
+    	}
+    	
+    }
+    	
+    	boolean intersects(Car car) {
+    		 if ((frogY > car.getY() && frogY < car.getY()+50) &&
+    		                (frogX > car.getX() && frogX < car.getX()+car.getSize())) {
+    		   return true;
+    		  }
+    		 else  {
+    		  return false;
+    		 }
     	
     }
     
