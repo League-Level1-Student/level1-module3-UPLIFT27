e15d49c8d17b671976951a0e2ea4cb5ceab45a7b
Sashrik Selva <sashrikselva@gmail.com>
2024-02-01 18:28:01 -0800

    continue with GridWorld

---
+++ src/_99_extra/_04_gridworld/Gridworld.java
@@ -0,0 +1,59 @@
+package _99_extra._04_gridworld;
+
+import java.awt.Color;
+
+import info.gridworld.actor.Bug;
+import info.gridworld.actor.Flower;
+import info.gridworld.grid.Location;
+import info.gridworld.world.World;
+
+public class Gridworld {
+
+//world
+	
+	
+
+	
+	
+
+
+	
+	
+	
+	
+	
+public static void main (String args[])
+		{
+	World world = new World();
+	
+	world.show();
+	world.add(10, bug);
+	
+	
+	Bug bug = new Bug();
+	
+	
+bug.setColor(Color.red);
+	
+	
+	
+
+	bug.turn();
+	
+	Flower flower = new Flower();
+	
+	flower
+	
+	//flower.setColor();
+	
+	Location location = new Location(10,10);
+	
+	
+		}
+	
+	
+	
+	
+	
+	
+}
